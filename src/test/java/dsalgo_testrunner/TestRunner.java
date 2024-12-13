1150a9c4ecc08dc59426d28b9c5cf12fe2491d0b
divyamadhuri14 <divyamadhuri.m@gmail.com>
2024-12-13 13:21:22 -0500

    hooksand config andwebdriver

--- src/test/java/dsalgo_testrunner/TestRunner.java
+++ src/test/java/dsalgo_testrunner/TestRunner.java
@@ -1,5 +1,36 @@
 package dsalgo_testrunner;
+    import org.junit.runner.RunWith;
+	import org.testng.annotations.DataProvider;
+	import org.testng.annotations.Parameters;
 
-public class TestRunner {
+	import io.cucumber.junit.Cucumber;
+	import io.cucumber.testng.AbstractTestNGCucumberTests;
+	import io.cucumber.testng.CucumberOptions;
 
-}
+
+
+	//@RunWith(Cucumber.class) //Junit execution
+
+		@CucumberOptions(
+				plugin = {"pretty", "html:target/dsAlgoReport.html"}, //reporting purpose
+				monochrome=false,  //console output color
+				tags =" @Tree", //tags from feature file
+				features = {"src/test/resources/dsalgo_features"}, //location of feature files
+				glue= {"dsalgo_stepdefinitions","dsalgo_hooks"})//location of step definition files
+
+
+		public class TestRunner extends AbstractTestNGCucumberTests {
+			
+			@Override
+		    @DataProvider(parallel =false)
+		    public Object[][]  scenarios() {
+						
+				return super.scenarios();
+				
+		    }
+		}
+
+
+	
+
+
